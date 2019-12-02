5c70b361ab5fff839b1a846456a08cb77b3960df
ttatsato <t-satoh@alby.co.jp>
2019-12-02 16:38:53 +0900

    axios追加

--- pages/index.vue
+++ pages/index.vue
@@ -36,41 +36,37 @@ import { Component, Vue } from 'vue-property-decorator'
 })
 export default class Index extends Vue {
   title: string = 'This is a message'
-  test () : void {
+  async test () : Promise<void> {
+    console.log(await this.$axios.get('/'));
     alert('test')
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass">
+  .container
+    margin: 0 auto
+    min-height: 100vh
+    display: flex
+    justify-content: center
+    align-items: center
+    text-align: center
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .title
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif
+    display: block
+    font-weight: 300
+    font-size: 100px
+    color: #35495e
+    letter-spacing: 1px
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle
+    font-weight: 300
+    font-size: 42px
+    color: #526488
+    word-spacing: 5px
+    padding-bottom: 15px
 
-.links {
-  padding-top: 15px;
-}
+  .links
+    padding-top: 15px
 </style>
